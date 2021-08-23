14d610440d645b28b6da18feae4b968d9afdd7e7
AngeloPF69 <88748745+AngeloPF69@users.noreply.github.com>
2021-08-23 18:17:47 +0100

    Update turtle.lua
    finishing local function up([Blocks=1])

--- turtle.lua
+++ turtle.lua
@@ -1,7 +1,7 @@
 local function forward(Blocks)
   Blocks = Blocks or 1
   
-  for i=1, Blocks do
+  for i = 1, Blocks do
     if not turtle.forward() then return false end
   end
   return true
@@ -10,8 +10,17 @@ end
 local function back(Blocks)
   Blocks = Blocks or 1
   
-  for i=1, Blocks do
+  for i = 1, Blocks do
     if not turtle.back() then return false end
+  end
+  return true
+end
+
+local function up(Blocks)
+  Blocks = Blocks or 1
+  
+  for i = 1, Blocks do
+    if not turtle.up() then return false end
   end
   return true
 end
