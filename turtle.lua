54c216beaba20eb47b0738792a46b907034aa931
AngeloPF69 <88748745+AngeloPF69@users.noreply.github.com>
2021-08-23 18:12:47 +0100

    Update turtle.lua
    local function back([Blocks=1]) added

--- turtle.lua
+++ turtle.lua
@@ -6,3 +6,12 @@ local function forward(Blocks)
   end
   return true
 end
+
+local function back(Blocks)
+  Blocks = Blocks or 1
+  
+  for i=1, Blocks do
+    if not turtle.back() then return false end
+  end
+  return true
+end
