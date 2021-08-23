1b3fd4b6c0d8896dd3030baba7d9398c0acd8199
Angelo Pereira Freitas <angelopfma@gmail.com>
2021-08-23 22:37:09 +0100

    Update turtle.lua
    added turn

--- turtle.lua
+++ turtle.lua
@@ -47,8 +47,35 @@ end
 local function goRight(Blocks)
   Blocks = Blocks or 1
   
+  turtle.turnRight()
   for i= 1, Blocks do
     if not turtle.forward() then return false end
+  end
+  return true
+end
+
+local function goBack(Blocks)
+  Blocks = Blocks or 1
+  
+  turtle.turnRight()
+  turtle.turnRight()
+  for i = 1, Blocks do
+    if not turtle.forward() then return false end
+  end
+  return true
+end
+
+local function turn(sDir)
+  sDir =sDir or "back"
+  
+  if sDir == "back" then
+    turtle.turnRight()
+    turtle.turnRight()
+  else if sDir == "left" then
+    turtle.turnLeft()
+  else if sDir == "right" then
+    turtle.turnRight()
+  else return false
   end
   return true
 end
