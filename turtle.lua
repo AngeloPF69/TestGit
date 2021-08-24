6d715b536693c3d4af52c20e5b7336c099f45b5a
Angelo Pereira Freitas <angelopfma@gmail.com>
2021-08-24 09:52:07 +0100

    Update turtle.lua
    added function dig(Blocks, Side)

--- turtle.lua
+++ turtle.lua
@@ -83,3 +83,13 @@ local function turn(sDir)
   end
   return true
 end
+
+local function dig(Blocks, Side)
+  Blocks = Blocks or 1
+  
+  for i = 1, Blocks do
+    if not turtle.dig(Side) then return false end
+    if not forward() then return false end
+  end
+  return true
+end
