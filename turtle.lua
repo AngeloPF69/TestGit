0be27dd233cf3a2fdce2143084a3a55b95bd60ba
AngeloPF69 <angelopfma@gmail.com>
2021-09-10 21:56:21 +0100

    cont

--- turtle.lua
+++ turtle.lua
@@ -1041,7 +1041,48 @@ function dropDown(nBlocks) --[[Drops nBlocks from selected slot and inventory in
 end
 
 ---- TEST AREA ------
---function digDir(sDir, nBlocks)
-print(digDir(-1))
+-- [x] itemName([Slot=Selected slot]) gets the item name from Slot.
+-- [x] inspectDir([sDir="forward]) turtle inspect block in sDir direction {"forward", "right", "back", "left", "up", "down"}.
+-- [x] suckDir([sDir="forward"][,count=all the items]) sucks items from sDir direction {"forward", "right", "back", "left", "up", "down"}.
+-- [x] attackDir([sDir="forward"]) Turtle attack in sDir direction {"forward", "right", "back", "left", "up", "down"}.
+-- [x] compareAbove([Blocks=1]) compare blocks above the turtle in a strait line with selected slot.
+-- [x] compareBelow([Blocks=1]) compare blocks below the turtle in a strait line with selected slot.
+-- [x] detectAbove([Blocks=1]) detects if exits Blocks above the turtle in a strait line forward or backwards.
+-- [x] detectBelow([Blocks=1]) detects if exits Blocks below the turtle in a strait line forward or backwards.
+-- [x] detectDir([Direction="forward"]) detects if there is a block in Direction { "forward", "right", "back", "left", "up", "down" }.
+-- [x] itemCount([selected slot/slot/inventory/item name]) counts items in inventory.
+-- [x] itemSelect([Slot/Item Name]) selects slot [1..16] or first item with Item Name, or the turtle selected slot.
+-- [x] Search([ItemName[, StartSlot=Selected Slot]]) Search inventory for ItemName, starting at StartSlot. 
+-- [x] placeBelow([Blocks=1]) places inventory selected Blocks in a strait line 1 block below the turtle and forward, and returns to initial position.
+-- [x] placeAbove([Blocks=1]) places inventory selected Blocks in a strait line 1 block above the turtle and forward, and returns to initial position.
+-- [x] placeRight([Blocks=1]) rotates turtle Right, places inventory selected Blocks in a strait line forward, and returns to initial position.
+-- [x] placeLeft([Blocks=1]) rotates turtle left, places inventory selected Blocks in a strait line forward, and returns to initial position.
+-- [x] place([Blocks=1]) places inventory selected Blocks in a strait line forward.
+-- [x] placeDown([Blocks=1]) places inventory selected Blocks in a strait line downward, and returns to initial position.
+-- [x] placeUp([Blocks=1]) places inventory selected Blocks in a strait line upward, and returns to initial position.
+-- [x] placeDir([Direction="forward"]) places inventory selected Block in Direction { "forward", "right", "back", "left", "up", "down" }.
+-- [x] digBack([Blocks=1]) rotates turtle back or not and dig Blocks forward.
+-- [x] digAbove([Blocks=1]) dig Blocks, 1 block above the turtle, and forward or backwards.
+-- [x] digBelow([Blocks=1]) dig Blocks, 1 block below the turtle, and forward or backwards.
+-- [x] digUp([Blocks=1]) dig Blocks upwards or downwards.
+-- [x] digDown([Blocks=1]) dig Blocks downwards or upwards.
+-- [x] digRight([Blocks=1]) rotates turtle Right or left and dig Blocks forward with tool.
+-- [X] digLeft([Blocks=1]) rotates turtle left or right and dig Blocks forward with tool.
+-- [x] dig([Blocks=1]) dig Blocks forward or backwards with tool.
+-- [x] digDir([Direction="forward"][, Blocks=1]) turtle digs in Direction direction Blocks.
+-- [X] turnDir([Direction="back"]) rotates turtle back, left or right.
+-- [x] goDir([Direction="forward][, nBlocks]) turtle goes in Direction { "forward", "right", "back", "left", "up", "down" } nBlocks until blocked.
+-- [x] goLeft(nBlocks) turns left or  right if nBlocks <0, and advances nBlocks until blocked.
+-- [x] goRight(nBlocks) turns right or left if nBlocks < 0, and advances nBlocks until blocked.
+-- [x] goBack(nBlocks) turns back or not if nBlocks < 0, and advances nBlocks until blocked.
+-- [x] down([Blocks=1]) moves the turtle down blocks, until it hits something.
+-- [x] up([Blocks=1]) moves the turtle up blocks, until it hits something.
+-- [x] back([Blocks=1]) moves the turtle backwards blocks, until it hits something.
+-- [x] forward([Blocks=1]) moves the turtle forward blocks, until it hits something.
+-- [x] dropDir([sDir="forward"][, nBlocks=1]) drops nBlocks from selected slot and inventory in the world in front, up or down the turtle.
+-- [x] drop(nBlocks) drops nBlocks from selected slot and inventory in the world in front of the turtle.
+-- [x] dropUp(nBlocks) drops nBlocks from selected slot and inventory in the world upwards.
+-- [x] dropDown(nBlocks) drops nBlocks from selected slot and inventory in the world downwards.
+
 
 
